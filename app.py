52cc2ac874fd27cc2aa9918da54cf0033988de2a
Sahil181045 <sahilkadam257@gmail.com>
2023-05-02 20:13:01 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import requests
-import numpy as np
+import os
 from PIL import Image
 from model import get_caption_model, generate_caption
 
@@ -9,20 +8,16 @@ from model import get_caption_model, generate_caption
 def get_model():
     return get_caption_model()
 
+
 caption_model = get_model()
 
-uploaded_file = st.file_uploader("Choose a file")
+st.title("Image Caption Generator")
+uploaded_file = st.file_uploader("Upload an Image")
+
 if uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img)
-    # img = np.array(img)
-    pred_caption = generate_caption(uploaded_file.name, caption_model)
+    img.save("image.jpg")
+    pred_caption = generate_caption("image.jpg", caption_model)
     st.write(pred_caption)
-
-# if (img_url != "") or (img_url != None):
-#     img = Image.open(requests.get(img_url, stream=True).raw)
-#     st.image(img)
-
-#     img = np.array(img)
-#     pred_caption = generate_caption(img, caption_model)
-#     st.write(pred_caption)
+    os.remove("image.jpg")
